97802cf23af33ceb521205a93a8b70909399e91c
Ancor Gonzalez Sosa <ancor@suse.de>
2014-01-07 14:30:33 +0100

    Version 0.0.3

--- init.rb
+++ init.rb
@@ -17,7 +17,7 @@ Redmine::Plugin.register :due_subtask do
   name 'Due subtask custom field'
   author 'Ancor Gonzalez Sosa'
   description 'Enable the usage of a custom field in issues to automatically calculate the next important date according to due time and subtasks'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/openSUSE-Team/redmine-due-subtask'
   author_url 'https://github.com/ancorgs'
   requires_redmine :version_or_higher => '2.0.0'
